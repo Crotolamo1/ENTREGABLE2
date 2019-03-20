8a0e0f55d6a95a4a29f9784f467ff10ef3831eb9
Jtorome <39070265+Jtorome@users.noreply.github.com>
2019-03-19 22:05:34 -0500

    .

--- src/entrega2/Conductor.java
+++ src/entrega2/Conductor.java
@@ -4,15 +4,28 @@ package entrega2;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Conductor {
+public class Conductor extends Persona {
     
     List<Conductor> DriverList = new ArrayList<Conductor>();
     
-    private int ServiceNumber=0;
+    private int ServicesNumber=0;
     private float AccumulatedRating=0;
     private ArrayList<Vehiculo> VehiclesList=new ArrayList<Vehiculo>();
     private ArrayList<Servicio> ServiceListDri=new ArrayList<Servicio>();
     private ArrayList<Servicio> CurrentService=new ArrayList<Servicio>();
     private ArrayList<Pasajero> UnqualifiedRider=new ArrayList<Pasajero>();
     
+    public Conductor(String email, String password, String name, String cellphone){
+        super(email, password, name, cellphone);
+    }
+    
+    public Conductor(String email, String password, String name, String cellphone, int servicesnumber, float accumulatedrating){
+        super(email, password, name, cellphone);
+        this.ServicesNumber=servicesnumber;
+        this.AccumulatedRating=accumulatedrating;
+    }
+    
+    public void SetServicesNumber(){
+        
+    }
 }
